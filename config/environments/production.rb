ba5d1cc8fe7cd22368be9ed32418390f005b9e95
Uy Binh <uybinh@gmail.com>
2018-07-30 22:56:32 +0700

    Update production mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,15 +70,18 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
    # Setup the mailer config
+
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'yourdomain.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
+  host = 'odin-flight-booker-hot-summer.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 
